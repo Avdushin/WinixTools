8a0fd4d1f181de1fecfc179fe83495774836d81a
Avdushin <avdushinbeaver1@gmail.com>
2022-07-16 21:47:09 +0300

    Winless folder fixed

---
+++ Winless/go.mod
@@ -0,0 +1,11 @@
+module ananazz
+
+go 1.18
+
+require github.com/logrusorgru/aurora v2.0.3+incompatible
+
+require (
+	github.com/gookit/color v1.5.1 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+)
---
+++ Winless/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/gookit/color"
+)
+
+func main() {
+	ls(nil)
+}
+
+func ls(err error) error {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			color.Green.Printf(color.Bold.Sprintf("%s ", file.Name()))
+		} else if !file.IsDir() {
+			fmt.Printf("%s ", file.Name())
+		}
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	if err != nil {
+		log.Print(err)
+	}
+	return nil
+}
